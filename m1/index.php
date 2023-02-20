0759dd9039c415aa7a09a40474b11a3331028dd4
Sara Romão <sara-romao@live.com>
2023-02-19 23:55:36 -0300

    feat:intro variáveis

---
+++ m1/index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+//Declarar variáveis
+
+$nome = "Sol";
+
+$idade = 1000;
+
+echo $idade;
+echo "<br>";
+
+
+//concatenar
+
+echo $nome . $idade;
+echo "<br>";
+//ou
+
+echo $nome." ".$idade;
+echo "<br>";
+
+//aspas duplas junta variáveis e texto
+echo "Nome: $nome e idade $idade";
+
+
+
+?>
