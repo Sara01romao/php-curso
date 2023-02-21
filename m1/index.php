54773e58d0a4b150ae5c4d5c401cdf1d6d4cfc42
Sara Romão <sara-romao@live.com>
2023-02-20 22:51:56 -0300

    feat: arrays

--- m1/index.php
+++ m1/index.php
@@ -10,6 +10,7 @@ $idade = 1000;
 echo $idade;
 echo "<br>";
 
+/*************************************************************************/
 
 //concatenar
 
@@ -22,6 +23,29 @@ echo "<br>";
 
 //aspas duplas junta variáveis e texto
 echo "Nome: $nome e idade $idade";
+echo "<br>";
+echo "<br>";
+
+
+/*************************************************************************/
+
+
+//Arrays
+
+$frutas = ['uva', 'limão', 'goiaba'];
+
+echo $frutas[0];
+echo "<br>";
+echo "<br>";
+
+//array Spread (7.4)
+$frutas2 = [...$frutas, 'caqui'];
+
+echo $frutas2[3];
+echo "<br>";
+
+print_r($frutas2); //ver itens no array
+
 
 
 
