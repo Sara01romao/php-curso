0e8b7f1f0495ba7a357cb4b0891e94a511b49917
Sara Romão <sara-romao@live.com>
2023-02-21 22:59:39 -0300

    feat: if else e ternario

---
+++ m2-condicionais-loops/index.php
@@ -0,0 +1,25 @@
+<?php
+
+
+//IF e ELSE
+
+$idade = 5;
+
+
+if($idade >=18){
+    echo "Maior de idade";
+
+}else{
+    echo "Menor de idade";
+}
+
+echo "<br>";
+
+//TERNÁRIO CONDICIONAL - (condição)?resultado positivo : resutado negativo
+
+echo ($idade < 18) ? "Sim" : "Não";
+
+
+
+
+?>
