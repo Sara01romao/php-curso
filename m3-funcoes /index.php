687aea6429c1b051b2cb9b2e685055cce1fd6044
Sara Romão <sara-romao@live.com>
2023-02-28 22:51:54 -0300

    feat:função parametro

--- m3-funcoes /index.php
+++ m3-funcoes /index.php
@@ -8,4 +8,21 @@ function subsequente(){
 
 
 subsequente();
+
+
+//parametros
+
+function somar($num1, $num2){
+
+    $total = $num1 + $num2;
+
+    return $total;
+
+}
+
+$soma = somar(10, 2);//argumentos
+
+echo "Total: $soma";
+
+
 ?>
