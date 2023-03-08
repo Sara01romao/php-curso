cd13bfd85b464fb957aea012820297f0d5df1717
Sara Romão <sara-romao@live.com>
2023-03-07 22:18:32 -0300

    feat: valor padrão

--- m3-funcoes /index.php
+++ m3-funcoes /index.php
@@ -24,5 +24,18 @@ $soma = somar(10, 2);//argumentos
 
 echo "Total: $soma";
 
+echo "<br>";
+echo "<br>";
+
+
+
+//pametros opcionais - valor padrão, caso não receba outro valor
+
+function soma2($n1, $n2, $n3=0){
+    return $n1 + $n2 +$n3;
+}
+
+$soma2Resultado= soma2(1, 2);
+echo $soma2Resultado;
 
 ?>
