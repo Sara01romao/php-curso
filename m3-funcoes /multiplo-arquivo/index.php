98ab1733da516765ac2c18d8ca7c7f536386e17c
Sara Romão <sara-romao@live.com>
2023-04-16 23:09:27 -0300

    require e include

--- m3-funcoes /multiplo-arquivo/index.php
+++ m3-funcoes /multiplo-arquivo/index.php
@@ -2,7 +2,11 @@
 
 //função para usar arquivo, importar arquivo
 
-require('header.php'); 
+require('header.php');
+
+require_once('header.php');//importa uma única vez 
+
+include('header.php');//executa o código mesmo com erro
 
 
 
