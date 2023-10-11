e44c48503934442b793bbb7a4ccf5a9015ada094
Sara Romão <sara-romao@live.com>
2023-10-11 10:20:17 -0300

    feat: form

--- m3-funcoes /multiplo-arquivo/header.php
+++
@@ -1,2 +0,0 @@
-<h1>Título Header</h1>
-<hr>
--- m4-formularo-validacao/upload.php
+++
@@ -1,6 +0,0 @@
-
-<form method="POST" action="receber.php" enctype="multipart/form-data">
-    <input type="file" name="arquivo"/>
-    <input type="submit" name="Enviar"/>
-
-</form>
