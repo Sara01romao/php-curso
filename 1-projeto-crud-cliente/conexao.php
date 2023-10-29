e11fc3bc0cc5df81050f95d2e2ecbc0f49187c28
Sara Romao <sara-romao@live.com>
2023-10-29 11:47:14 -0300

    feat:conexao

---
+++ 1-projeto-crud-cliente/conexao.php
@@ -0,0 +1,17 @@
+
+<?php
+
+$host = "localhost";
+$db = "clientes";
+$user = "root";
+$pass= "";
+
+
+
+$mysqli= new mysqli($host, $user, $pass, $db);
+
+if($mysqli -> connect_errno){
+    die("Falha na conexão com o banco de dados");
+}
+
+?>
